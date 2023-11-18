f03f2107bc2601d69e2a5484051c6bbdfc7a3d21
VJVIV <94681456+VJVIV@users.noreply.github.com>
2023-11-18 11:24:29 +0100

    First commit with gitignore

---
+++ main.py
@@ -0,0 +1,117 @@
+import numpy as np
+import pandas as pd
+from sklearn.preprocessing import MinMaxScaler
+import tensorflow as tf
+from keras.models import Sequential
+from keras.layers import Dense, LSTM, Dropout
+from keras.callbacks import EarlyStopping, ModelCheckpoint
+from keras.optimizers import Adam
+import matplotlib.pyplot as plt
+
+def train_val_test_split(df):
+    df['Date'] = pd.to_datetime(df['Date'])
+    train_date_end = '2023-07-31'
+    lookback_for_val = '2023-06-01'
+    val_date_end = '2023-08-15'
+    lookback_for_test = '2023-06-16'
+
+    train_df = df[df['Date'] <= train_date_end].reset_index(drop=True)
+    val_df = df[(df['Date'] >= lookback_for_val) & (df['Date'] <= val_date_end)].reset_index(drop=True)
+    test_df = df[df['Date'] >= lookback_for_test].reset_index(drop=True)
+
+    print("Training Data:")
+    print(train_df)
+    print("Validation Data:")
+    print(val_df)
+    print("Testing Data:")
+    print(test_df)
+
+    for df in [train_df, val_df, test_df]:
+        df.drop(columns=['Date'], inplace=True)
+    return train_df, val_df, test_df
+
+def standardize(train, val, test):
+    scaler = MinMaxScaler()
+    train_scaled = scaler.fit_transform(train)
+    val_scaled = scaler.transform(val)
+    test_scaled = scaler.transform(test)
+    return train_scaled, val_scaled, test_scaled, scaler
+
+def windows(data, lookback, lookfront):                    
+    X, y = [], []
+    for i in range(len(data) - lookback - lookfront):
+        X.append(data[i:i+lookback])                            #Multivariate - all 11 features
+        y.append(data[i+lookback:i+lookback+lookfront, 0])      #Predict only BTC close price (first feature)
+    return np.array(X), np.array(y)
+
+
+if __name__ == "__main__":
+    df = pd.read_csv(r'C:\Users\vijay\Desktop\Personal_projects\BTC-pred-model\merged_preprocessed_data.csv')
+    train_data, val_data, test_data = train_val_test_split(df) 
+    train_scaled, val_scaled, test_scaled, scaler = standardize(train_data, val_data, test_data) 
+
+    lookback_steps = 60                                         #Lookback last x days 
+    predict_steps = 1                                           #and predict next y days
+    X_train, y_train = windows(train_scaled, lookback=lookback_steps, lookfront=predict_steps)
+    print('X train shape:', X_train.shape, 'y train shape:', y_train.shape)
+    X_val, y_val = windows(val_scaled, lookback=lookback_steps, lookfront=predict_steps)
+    print('X val shape:', X_val.shape, 'y val shape:', y_val.shape)
+    X_test, y_test = windows(test_scaled, lookback=lookback_steps, lookfront=predict_steps)
+    print('X test shape:', X_test.shape, 'y test shape:', y_test.shape)
+
+    # Creating LSTM model
+    custom_optimizer = Adam(learning_rate=0.0001)
+    early_stopping = EarlyStopping(monitor='val_loss', patience=2)
+    checkpoint_callback = ModelCheckpoint("best_model.h5", save_best_only=True, monitor='val_loss', 
+                                          mode='min', verbose=1)
+   
+    model = Sequential()
+    model.add(LSTM(50, return_sequences=True, input_shape=(X_train.shape[1], X_train.shape[2])))
+    model.add(LSTM(40, return_sequences=True))
+    model.add(LSTM(30, return_sequences=False))
+    model.add(Dense(20))
+    model.add(Dense(y_train.shape[1]))
+    model.compile(optimizer=custom_optimizer, loss='mse')
+
+    # Train the model
+    history = model.fit(X_train, y_train, batch_size=16, epochs=20, shuffle=False, validation_data=(X_val, y_val),
+                        validation_batch_size = 1, callbacks=[early_stopping, checkpoint_callback])
+    
+    # Plot training & validation loss
+    train_loss = history.history['loss']
+    val_loss = history.history['val_loss']
+    plt.plot(train_loss, label='Training Loss')
+    plt.plot(val_loss, label='Validation Loss')
+    plt.title('Training and Validation Loss')
+    plt.xlabel('Epoch')
+    plt.ylabel('Loss')
+    plt.legend()
+    plt.show()
+
+    #Test the model
+    predictions = model.predict(X_test)
+    print(predictions.shape)
+    print(y_test.shape)
+
+    for i in range(predictions.shape[0]):
+        prediction = predictions[i].reshape(-1,1)
+        y_test_compare = y_test[i].reshape(-1,1)
+        print('Shape of prediction -', prediction.shape, 'and shape of target -', y_test_compare.shape)
+
+        repeat_pred = np.repeat(prediction, X_train.shape[2], axis=1)
+        repeat_y = np.repeat(y_test_compare, X_train.shape[2], axis=1)
+        print('Shape of repeat pred -', repeat_pred.shape, 'and shape of repeat y -', repeat_y.shape)
+
+        pred_price = scaler.inverse_transform(repeat_pred)[:, 0].reshape(-1,1)
+        org_price = scaler.inverse_transform(repeat_y)[:, 0].reshape(-1,1)
+        print('Shape of preds -', pred_price.shape, 'and shape of orginal price -', org_price.shape)
+        
+        print('Original price -', org_price)
+        print('Predicted price -', pred_price)
+
+    
+
+
+
+
+    
---
+++ pre_process.py
@@ -0,0 +1,99 @@
+import os
+import numpy as np
+import pandas as pd
+from copy import deepcopy
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+def merge_files_directory(path, master_df=None):
+    if master_df is None:
+        master_df = pd.DataFrame()
+
+    csv_files = [file for file in os.listdir(path) if file.endswith('.csv')]
+    for file in csv_files:
+        file_path = os.path.join(path, file)
+        df = pd.read_csv(file_path)
+
+        file_prefix = file.split('.')[0][:3]
+        print(file_prefix)
+        df.columns = [f"{file_prefix}_{col}" if col != "Date" else col for col in df.columns]
+        if master_df.empty:
+            master_df = deepcopy(df)
+        else:
+            master_df = pd.merge(master_df, df, on='Date', how='outer')
+
+    return master_df
+
+def pre_process(master_df):
+    #Dropping certain columns
+    columns_to_drop = [col for col in master_df.columns if 'Vol' in col or 'Change' in col]
+    master_df.drop(columns=columns_to_drop, inplace=True)
+
+    #Sorting by date
+    master_df['Date'] = pd.to_datetime(master_df['Date'], format='%m/%d/%Y')
+    master_df = master_df[master_df['Date'].dt.year >= 2021]                    #Focus on more recent years
+    master_df.sort_values(by='Date', ascending=True, inplace=True)
+    master_df.reset_index(drop=True, inplace=True)
+
+    #Formating columns to float
+    print('Column dtypes:')
+    print(master_df.dtypes)
+    numeric_columns = master_df.select_dtypes(include=['object']).columns
+    for col in numeric_columns:
+        master_df.loc[:, col] = master_df[col].str.replace(",", "").astype(float)
+
+    #Handling NaN values
+    nan_count = master_df.isna().sum().sum()
+    print('Number of NaNs -', nan_count)
+    master_df = master_df.fillna(method='ffill')                                #Some variables miss values over weekend
+    master_df = master_df.dropna()                                              #Starting rows, no previous val for ffill
+    print('Number of NaNs -', master_df.isna().sum().sum())
+
+    #Correlation matrix
+    master_df_date_removed = deepcopy(master_df)
+    master_df_date_removed.drop(columns='Date', inplace=True)
+    correlation_matrix = master_df_date_removed.corr()
+    plt.figure(figsize=(8, 6))  # Set the figure size
+    sns.heatmap(correlation_matrix, cmap='coolwarm', vmin=-1, vmax=1)
+    plt.title('Correlation Matrix')
+    plt.show()
+
+    #Keeping most interesting features after correlation analysis
+    columns_to_drop = [col for col in master_df.columns if 'Date' not in col and 'BTC_Price' not in col
+                       and 'BTC_Open' not in col and 'BTC_High' not in col and 'BTC_Low' not in col
+                       and 'ETH_Price' not in col and 'Nas_Price' not in col and 'S&P_Price' not in col
+                       and 'Sil_Price' not in col and 'USD_Price' not in col]
+    master_df.drop(columns=columns_to_drop, inplace=True)
+
+    #BTC price plot
+    plt.plot(master_df.index, master_df['BTC_Price'])
+    plt.xlabel('Days (Jan 04 2021 - Sep 15 2023)')
+    plt.ylabel('BTC price')
+    plt.title('BTC price plot')
+    plt.grid(True)
+    plt.show()
+
+    return master_df
+
+
+if __name__ == "__main__":
+    raw_data_path = r'C:\Users\vijay\Desktop\Personal_projects\BTC-pred-model\raw_data'
+    data_path = r'C:\Users\vijay\Desktop\Personal_projects\BTC-pred-model'
+
+    master_df = merge_files_directory(raw_data_path)
+    print('Master df after merge:')
+    print(master_df.shape)
+
+    master_df_pre_process = pre_process(master_df)
+    print('Master df after pre-processing:')
+    print(master_df_pre_process.shape)
+
+    merged_pre_processed_data_path = os.path.join(data_path, 'merged_preprocessed_data.csv')
+    master_df_pre_process.to_csv(merged_pre_processed_data_path, index=False)
+
+    
+
+
+
+
+
